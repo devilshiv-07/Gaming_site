6d9fb482de7fad793b71ab2ef5f08899ea58e09d
Shivank Tripathi <182352677+devilshiv-07@users.noreply.github.com>
2025-02-12 10:30:37 +0530

    setup is done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-5xl text-violet-300 font-bold'>Welcome to Awwards</h1>
+    </main>
+  )
+}
+
+export default App
