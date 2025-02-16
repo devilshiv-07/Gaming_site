55757bad0beb0cab9e8edcffa6d2dd6e15bfc6b4
Shivank Tripathi <182352677+devilshiv-07@users.noreply.github.com>
2025-02-16 17:15:11 +0530

    Story section completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ title, id , leftIcon, containerClass, rightIcon}) => {
   return (
-    <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 text-black ${containerClass}`}>
+    <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 text-black bg-blue-50 ${containerClass}`}>
         {leftIcon}
 
         <span className='relative inline-flex overflow-hidden font-general text-xs uppercase'>
